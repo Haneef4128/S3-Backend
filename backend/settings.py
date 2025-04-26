6013654054f2c97ea485ef63ef24e72cb76a7bfb
Haneef <mdhaneef2001@gmail.com>
2025-04-26 13:27:35 +0530

    Commmit

--- backend/settings.py
+++ backend/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-n%6tpnw*i0#x!k27lf8b-t87)gx2q2jlj!z$hyzs5m7&adznw0'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 # ALLOWED_HOSTS = ["*"]
 
@@ -183,3 +183,5 @@ CORS_ALLOW_HEADERS = [
 ]
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
